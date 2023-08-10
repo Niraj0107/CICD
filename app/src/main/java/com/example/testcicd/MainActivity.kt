055d32dab618734d9162e84ad52271a3ec63033d
Niraj0107 <106701884+Niraj0107@users.noreply.github.com>
2023-08-10 14:05:32 +0530

    Update via Python script

--- app/src/main/java/com/example/testcicd/MainActivity.kt
+++ app/src/main/java/com/example/testcicd/MainActivity.kt
@@ -6,7 +6,7 @@ import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
-    private var companyId: Int = 220
+    private var companyId: Int = 225
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
