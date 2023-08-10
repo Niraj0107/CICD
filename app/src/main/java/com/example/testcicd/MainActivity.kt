2a3e1e2abbf0a6531cd2ff7a66a3a5b47333326b
Niraj <Niraj.kumar@tissatech.com>
2023-08-10 17:36:03 +0530

    Build and Deploy APK 36

--- app/src/main/java/com/example/testcicd/MainActivity.kt
+++ app/src/main/java/com/example/testcicd/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.testcicd
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,8 +11,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         Log.d("TAG", "onCreate: test ")
+        Toast.makeText(applicationContext, "Company ID $companyId", Toast.LENGTH_LONG).show()
     }
 }
 
